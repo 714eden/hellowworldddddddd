ff71e8f0b140c00d0e6741d8b237cb16881d1524
714eden <42377445+714eden@users.noreply.github.com>
2019-03-06 15:32:09 +0800

    app

--- routes.js
+++ routes.js
@@ -18,6 +18,38 @@ var appRouter = function (app) {
     res.status(200).send(data);
   });
 
+  app.get("/student", function (req, res) {
+    var data = ({
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      id: faker.random.uuid()
+    });
+    res.status(200).send(data);
+  });
+
+  app.get("/students/:num", function (req, res) {
+    var users = [];
+    var num = req.params.num;
+ 
+    if (isFinite(num) && num  > 0 ) {
+      for (i = 0; i <= num-1; i++) {
+        users.push({
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            username: faker.internet.userName(),
+            email: faker.internet.email(),
+            id: faker.random.uuid()
+         });
+      }
+ 
+      res.status(200).send(users);
+     
+    } else {
+      res.status(400).send({ message: 'invalid number supplied' });
+    }
+  });
  app.get("/users/:num", function (req, res) {
    var users = [];
    var num = req.params.num;
